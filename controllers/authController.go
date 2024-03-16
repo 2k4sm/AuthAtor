be51ee1b196c2496cc952552d9dc862efff13ed5
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-03-16 22:10:57 +0530

    adds more authController Functions.

--- controllers/authController.go
+++ controllers/authController.go
@@ -17,6 +17,7 @@ func Login(c *gin.Context) {
 
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
 	}
 
 	existingUser := models.User{}
@@ -57,3 +58,78 @@ func Login(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"success": "user logged in"})
 }
 
+func Signup(c *gin.Context) {
+	user := models.User{}
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	existingUser := models.User{}
+
+	models.DB.Where("email = ?", user.Email).First(&existingUser)
+
+	if existingUser.ID != 0 {
+		c.JSON(http.StatusConflict, gin.H{"error": "user already exists"})
+		return
+	}
+
+	var errorHash error
+	user.Password, errorHash = utils.GenerateHashPassword(user.Password)
+
+	if errorHash != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "could not generate hash password"})
+		return
+	}
+
+	models.DB.Create(&user)
+	c.JSON(http.StatusOK, gin.H{"success": "user created"})
+}
+
+func Home(c *gin.Context) {
+	cookie, err := c.Cookie("token")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	claims, err := utils.ParseToken(cookie)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	if claims.Role != "user" && claims.Role != "admin" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": "home page", "role": claims.Role})
+}
+
+func Premium(c *gin.Context) {
+	cookie, err := c.Cookie("token")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	claims, err := utils.ParseToken(cookie)
+	if err != nil {
+		c.JSON(401, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	if claims.Role != "admin" {
+		c.JSON(401, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	c.JSON(200, gin.H{"success": "premium page", "role": claims.Role})
+}
+
+func Logout(c *gin.Context) {
+	c.SetCookie("token", "", -1, "/", "localhost", false, true)
+	c.JSON(200, gin.H{"success": "user logged out"})
+}
